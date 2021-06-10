35e3349a99c10fd55fb5c91d213e438eca9e6c65
IvanSampere <sampere21413@iesmarenostrum.com>
2021-06-10 17:25:12 +0200

    Cambios Examen Entornos 20/21 Ivan Sampere

--- ClassLibrary1/ClassLibrary1/Examen3EV.cs
+++ ClassLibrary1/ClassLibrary1/Examen3EV.cs
@@ -2,38 +2,42 @@
 
 namespace Examen3EV_NS
 {
-    public class estNot  // esta clase nos calcula las estadísticas de un conjunto de notas 
+    public class estNotISQ2021  // esta clase nos calcula las estadísticas de un conjunto de notas 
     {
-        public int sus;  // Suspensos
-        public int apr;  // Aprobados
-        public int not;  // Notables
-        public int sbr;  // Sobresalientes
+        private int suspenso;  // Suspensos
+        private int aprobados;  // Aprobados
+        private int notable;  // Notables
+        private int sobresaliente;  // Sobresalientes
 
-        public double media; // Nota media
+        private double media; // Nota media
+
+        // Getters y Setters
+        public int Suspenso { get => suspenso; set => suspenso = value; }
+
+        public int Aprobados { get => aprobados; set => aprobados = value; }
+        public int Notable { get => notable; set => notable = value; }
+        public int Sobresaliente { get => sobresaliente; set => sobresaliente = value; }
+
+        public double Media { get => media; set => media = value; }
 
         // Constructor vacío
-        public estNot()
+        public estNotISQ2021()
         {
-            sus = apr = not = sbr = 0;  // inicializamos las variables
-            media = 0.0;
+            // inicializamos las variables
+            this.suspenso = 0;
+            this.aprobados = 0;
+            this.notable = 0;
+            this.sobresaliente = 0;
+            this.media = 0.0;
         }
 
         // Constructor a partir de un array, calcula las estadísticas al crear el objeto
-        public estNot(List<int> listnot)
+        public estNotISQ2021(List<int> listnot)
         {
-            media = 0.0;
+            this.media = 0.0;
 
-            for (int i = 0; i < listnot.Count; i++)
-            {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] > 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] > 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
+            RealizarMedia(listnot);
 
-                media = media + listnot[i];
-            }
-
-            media = media / listnot.Count;
         }
 
 
@@ -41,32 +45,38 @@ namespace Examen3EV_NS
         // calcula la media y el número de suspensos/aprobados/notables/sobresalientes
         //
         // El método devuelve -1 si ha habido algún problema, la media en caso contrario	
-        public double calcEst(List<int> listnot)
-        {                                 
-            media = 0.0;
+        public double CalcEst(List<int> listnot)
+        {
+            this.media = 0.0;
 
             // TODO: hay que modificar el tratamiento de errores para generar excepciones
             //
             if (listnot.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
                 return -1;
 
-            for (int i=0;i<10;i++)
+            for (int i = 0; i < 10; i++)
                 if (listnot[i] < 0 || listnot[i] > 10)      // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
-                return -1;
+                    return -1;
+
+            return RealizarMedia(listnot);
+        }
+
+        // Función para aumentar al contador de suspensos, aprobados, notable y sobresaliente según las notas.
+        public double RealizarMedia(List<int> listnot)
+        {
 
             for (int i = 0; i < listnot.Count; i++)
             {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] >= 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] >= 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
+                if (listnot[i] < 5) this.suspenso++;              // Por debajo de 5 suspenso
+                else if (listnot[i] >= 5 && listnot[i] < 7) this.aprobados++;// Nota para aprobar: 5
+                else if (listnot[i] >= 7 && listnot[i] < 9) this.notable++;// Nota para notable: 7
+                else if (listnot[i] > 9) this.sobresaliente++;         // Nota para sobresaliente: 9
 
-                media = media + listnot[i];
+                this.media = this.media + listnot[i];
             }
 
-            media = media / listnot.Count;
+            return this.media = this.media / listnot.Count;
 
-            return media;
         }
     }
 }
